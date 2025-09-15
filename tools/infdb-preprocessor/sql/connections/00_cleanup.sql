4aff756c40f7f9dedfd3609a88a73c8b8285c5fb
Patrick Buchenberg <patrick.buchenberg@tum.de>
2025-09-15 19:22:26 +0200

    added connection from buildings to streets

---
+++ tools/infdb-preprocessor/sql/connections/00_cleanup.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA IF NOT EXISTS {output_schema};
+DROP TABLE IF EXISTS {output_schema}.connections_buildings_to_ways;
---
+++ tools/infdb-preprocessor/sql/connections/02_create_connections_buildings_to_streets.sql
@@ -0,0 +1,1 @@
+SELECT {output_schema}.generate_connections_buildings_to_ways();
