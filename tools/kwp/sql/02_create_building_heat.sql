3825712d277423110177d9c5ce724b06bd7e5435
Patrick Buchenberg <patrick.buchenberg@tum.de>
2025-09-19 00:58:13 +0200

    added start and endpoint to kwp heat result

--- tools/kwp/sql/02_create_building_heat.sql
+++ tools/kwp/sql/02_create_building_heat.sql
@@ -1,6 +1,6 @@
 -- DROP TABLE IF EXISTS {output_schema}.buildings_heat_demand;
 CREATE TABLE {output_schema}.buildings_heat_demand AS
-    SELECT buildings_pylovo.*, entise_output.demand_heating, connections_buildings_to_ways.connection_geom
+    SELECT buildings_pylovo.*, entise_output.demand_heating, connections_buildings_to_ways.connection_geom, connections_buildings_to_ways.endpoint_geom
     FROM {input_schema_basedata}.buildings_pylovo
     JOIN {input_schema_ro-heat}.entise_output ON entise_output.building_objectid = buildings_pylovo.objectid
     JOIN {input_schema_ro-heat}.buildings_rc ON buildings_rc.building_objectid = buildings_pylovo.objectid
