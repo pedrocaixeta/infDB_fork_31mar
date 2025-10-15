412e70d7cf2ad3142ad772e8e5f1465215a4b93e
Patrick Buchenberg <patrick.buchenberg@tum.de>
2025-10-15 21:42:04 +0200

    clean startup script

--- startup.sh
+++ startup.sh
@@ -4,19 +4,14 @@
 # ----------------------------------------------------------------------
 # infDB Setup Script
 # ----------------------------------------------------------------------
-# Configure your instance name here
-BRANCH_NAME="develop"
-# ----------------------------------------------------------------------
 
-echo "=== Checkout branch '${BRANCH_NAME}' ==="
-git checkout "${BRANCH_NAME}"
-# git submodule update --init --recursive
-
-echo "=== Setup infDB (generate config files) ==="
 docker compose -f services/infdb-setup/compose.yml up
 
 echo "=== Run infDB ==="
 docker compose -f compose.yml up -d
+
+echo "=== Wait for core services to initialize ==="
+sleep 20
 
 echo "=== Run infDB-loader ==="
 docker compose -f tools/infdb-loader/compose.yml up
@@ -30,4 +25,4 @@ docker compose -f tools/ro-heat/compose.yml up
 echo "=== Run kwp ==="
 docker compose -f tools/kwp/compose.yml up
 
-echo "=== Done! Instance '${INSTANCE_NAME}' on branch '${BRANCH_NAME}' is ready. ==="
+echo "=== Done! Instance '${INSTANCE_NAME}' is ready. ==="
