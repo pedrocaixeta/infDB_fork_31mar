7d335578a6f764e55f316111fee2013ee130e65c
Ayasse, Carolin <carolin.ayasse@eins.tu-darmstadt.de>
2025-11-28 15:52:10 +0100

    minimal working version to assign buildings to streets and calulate linear heat density with every data

--- tools/linear-heat-density/main.py
+++ tools/linear-heat-density/main.py
@@ -7,7 +7,6 @@ Handles InfDB initialization, database connection, logging, and demo execution.
 # Import packages
 import os
 from infdb import InfDB
-from src import demo, linear_heat_density
 
 
 def main():
@@ -23,38 +22,28 @@ def main():
     infdb.log.info(f"Starting {infdb.get_toolname()} tool")
 
     try:
-        # ===========================================================
-        # Start your added python code in folder "src"
-        # ===========================================================
-        infdb.log.info("Running python code ...")
-        linear_heat_density.example_function(variable="Hello, InfDB!")
-
+        
         # ===========================================================
         # Start your added sql scripts in folder "sql"
         # ===========================================================
         infdb.log.info("Running SQL scripts ...")
         format_params = {
-            'input_streets_schema': infdb.get_config_value([infdb.get_toolname(), "data", "input", "streets", "schema"]),
-            'input_streets_table': infdb.get_config_value([infdb.get_toolname(), "data", "input", "streets", "table"]),
-            'input_streets_id': infdb.get_config_value([infdb.get_toolname(), "data", "input", "streets", "id_column"]),
-            'input_streets_geom': infdb.get_config_value([infdb.get_toolname(), "data", "input", "streets", "geom_column"]),
-            'input_buildings_schema': infdb.get_config_value([infdb.get_toolname(), "data", "input", "buildings", "schema"]),
-            'input_buildings_table': infdb.get_config_value([infdb.get_toolname(), "data", "input", "buildings", "table"]),
-            'input_buildings_id': infdb.get_config_value([infdb.get_toolname(), "data", "input", "buildings", "id_column"]),
-            'input_buildings_geom': infdb.get_config_value([infdb.get_toolname(), "data", "input", "buildings", "geom_column"]),
+            'buildings_to_streets_schema': infdb.get_config_value([infdb.get_toolname(), "data", "input", "buildings-to-streets", "schema"]),
+            'buildings_to_streets_table': infdb.get_config_value([infdb.get_toolname(), "data", "input", "buildings-to-streets", "table"]),
+            'streets_schema': infdb.get_config_value([infdb.get_toolname(), "data", "input", "streets", "schema"]),
+            'streets_table': infdb.get_config_value([infdb.get_toolname(), "data", "input", "streets", "table"]),
+            'streets_id': infdb.get_config_value([infdb.get_toolname(), "data", "input", "streets", "id-column"]),
+            'streets_geom': infdb.get_config_value([infdb.get_toolname(), "data", "input", "streets", "geom-column"]),
+            'heat_demand_schema': infdb.get_config_value([infdb.get_toolname(), "data", "input", "heat-demand", "schema"]),
+            'heat_demand_table': infdb.get_config_value([infdb.get_toolname(), "data", "input", "heat-demand", "table"]),
+            'heat_demand_id': infdb.get_config_value([infdb.get_toolname(), "data", "input", "heat-demand", "id-column"]),
+            'heat_demand_column': infdb.get_config_value([infdb.get_toolname(), "data", "input", "heat-demand", "heat-demand-column"]),
             'output_schema': infdb.get_config_value([infdb.get_toolname(), "data", "output", "schema"]),
             'output_table': infdb.get_config_value([infdb.get_toolname(), "data", "output", "table"]),
         }
         SQL_DIR = os.path.join("sql")   # add subfolders here if needed ("sql/subfolder")
         infdb.connect().execute_sql_files(SQL_DIR, format_params=format_params)
 
-        # ===========================================================
-        # Demonstrate database querying - remove or comment out if not needed
-        # ===========================================================
-        infdb.log.info("Running demo ...")
-        demo.sql_demo(infdb)
-        demo.database_demo(infdb)
-        demo.database_demo_sqlalchemy()
 
     except Exception as e:
         infdb.log.error(f"Something went wrong: {str(e)}")
