aef7071d062ca259da804565ea3a45eb664c59fa
bereba <beneharo.reveron-baecker@tum.de>
2025-12-19 08:40:16 +0100

    Temporary type casting within prepare_grid for buildings

--- tools/infdb-basedata/sql/buildings_sql/08_prepare_grid.sql
+++ tools/infdb-basedata/sql/buildings_sql/08_prepare_grid.sql
@@ -34,25 +34,25 @@ ADD COLUMN durchschnhhgroesse double precision,
 ADD COLUMN werterlaeuternde_zeichen text,
 -- zensus_2022_100m_gebaeude_typ_groesse
 ADD COLUMN insgesamt_gebaeude bigint,
-ADD COLUMN freiefh double precision,
-ADD COLUMN efh_dhh double precision,
-ADD COLUMN efh_reihenhaus double precision,
-ADD COLUMN freist_zfh double precision,
-ADD COLUMN zfh_dhh double precision,
-ADD COLUMN zfh_reihenhaus double precision,
-ADD COLUMN mfh_3bis6wohnungen double precision,
-ADD COLUMN mfh_7bis12wohnungen double precision,
-ADD COLUMN mfh_13undmehrwohnungen double precision,
-ADD COLUMN anderergebaeudetyp double precision,
+ADD COLUMN freiefh bigint,
+ADD COLUMN efh_dhh bigint,
+ADD COLUMN efh_reihenhaus bigint,
+ADD COLUMN freist_zfh bigint,
+ADD COLUMN zfh_dhh bigint,
+ADD COLUMN zfh_reihenhaus bigint,
+ADD COLUMN mfh_3bis6wohnungen bigint,
+ADD COLUMN mfh_7bis12wohnungen bigint,
+ADD COLUMN mfh_13undmehrwohnungen bigint,
+ADD COLUMN anderergebaeudetyp bigint,
 -- zensus_2022_100m_gebaeude_baujahr_mikrozensus
-ADD COLUMN vor1919 double precision,
-ADD COLUMN a1919bis1948 double precision,
-ADD COLUMN a1949bis1978 double precision,
-ADD COLUMN a1979bis1990 double precision,
-ADD COLUMN a1991bis2000 double precision,
-ADD COLUMN a2001bis2010 double precision,
-ADD COLUMN a2011bis2019 double precision,
-ADD COLUMN a2020undspaeter double precision;
+ADD COLUMN vor1919 bigint,
+ADD COLUMN a1919bis1948 bigint,
+ADD COLUMN a1949bis1978 bigint,
+ADD COLUMN a1979bis1990 bigint,
+ADD COLUMN a1991bis2000 bigint,
+ADD COLUMN a2001bis2010 bigint,
+ADD COLUMN a2011bis2019 bigint,
+ADD COLUMN a2020undspaeter bigint;
 
 -- Update with population data
 UPDATE {output_schema}.buildings_grid
@@ -63,39 +63,39 @@ WHERE buildings_grid.x_mp = pop.x_mp_100m
 
 -- Update with household size data
 UPDATE {output_schema}.buildings_grid
-SET durchschnhhgroesse = hh.durchschnhhgroesse::double precision,
-    werterlaeuternde_zeichen = hh.werterlaeuternde_zeichen
+SET durchschnhhgroesse = REPLACE(hh.durchschnhhgroesse, ',', '.')::double precision,
+    werterlaeuternde_zeichen = hh.werterlaeuternde_zeichen::text
 FROM {input_schema}.zensus_2022_100m_durchschn_haushaltsgroesse hh
 WHERE buildings_grid.x_mp = hh.x_mp_100m
   AND buildings_grid.y_mp = hh.y_mp_100m;
 
 -- Update with building type data
 UPDATE {output_schema}.buildings_grid
-SET insgesamt_gebaeude = bld.insgesamt_gebaeude::bigint,
-    freiefh = (CASE WHEN bld.freiefh IN ('-', '–') THEN 'NaN' ELSE bld.freiefh END)::double precision,
-    efh_dhh = (CASE WHEN bld.efh_dhh IN ('-', '–') THEN 'NaN' ELSE bld.efh_dhh END)::double precision,
-    efh_reihenhaus = (CASE WHEN bld.efh_reihenhaus IN ('-', '–') THEN 'NaN' ELSE bld.efh_reihenhaus END)::double precision,
-    freist_zfh = (CASE WHEN bld.freist_zfh IN ('-', '–') THEN 'NaN' ELSE bld.freist_zfh END)::double precision,
-    zfh_dhh = (CASE WHEN bld.zfh_dhh IN ('-', '–') THEN 'NaN' ELSE bld.zfh_dhh END)::double precision,
-    zfh_reihenhaus = (CASE WHEN bld.zfh_reihenhaus IN ('-', '–') THEN 'NaN' ELSE bld.zfh_reihenhaus END)::double precision,
-    mfh_3bis6wohnungen = (CASE WHEN bld.mfh_3bis6wohnungen IN ('-', '–') THEN 'NaN' ELSE bld.mfh_3bis6wohnungen END)::double precision,
-    mfh_7bis12wohnungen = (CASE WHEN bld.mfh_7bis12wohnungen IN ('-', '–') THEN 'NaN' ELSE bld.mfh_7bis12wohnungen END)::double precision,
-    mfh_13undmehrwohnungen = (CASE WHEN bld.mfh_13undmehrwohnungen IN ('-', '–') THEN 'NaN' ELSE bld.mfh_13undmehrwohnungen END)::double precision,
-    anderergebaeudetyp = (CASE WHEN bld.anderergebaeudetyp IN ('-', '–') THEN 'NaN' ELSE bld.anderergebaeudetyp END)::double precision
+SET insgesamt_gebaeude = (CASE WHEN bld.insgesamt_gebaeude IN ('-', '–', '') THEN '0' ELSE bld.insgesamt_gebaeude END)::bigint,
+    freiefh = (CASE WHEN bld.freiefh IN ('-', '–', '') THEN '0' ELSE bld.freiefh END)::bigint,
+    efh_dhh = (CASE WHEN bld.efh_dhh IN ('-', '–', '') THEN '0' ELSE bld.efh_dhh END)::bigint,
+    efh_reihenhaus = (CASE WHEN bld.efh_reihenhaus IN ('-', '–', '') THEN '0' ELSE bld.efh_reihenhaus END)::bigint,
+    freist_zfh = (CASE WHEN bld.freist_zfh IN ('-', '–', '') THEN '0' ELSE bld.freist_zfh END)::bigint,
+    zfh_dhh = (CASE WHEN bld.zfh_dhh IN ('-', '–', '') THEN '0' ELSE bld.zfh_dhh END)::bigint,
+    zfh_reihenhaus = (CASE WHEN bld.zfh_reihenhaus IN ('-', '–', '') THEN '0' ELSE bld.zfh_reihenhaus END)::bigint,
+    mfh_3bis6wohnungen = (CASE WHEN bld.mfh_3bis6wohnungen IN ('-', '–', '') THEN '0' ELSE bld.mfh_3bis6wohnungen END)::bigint,
+    mfh_7bis12wohnungen = (CASE WHEN bld.mfh_7bis12wohnungen IN ('-', '–', '') THEN '0' ELSE bld.mfh_7bis12wohnungen END)::bigint,
+    mfh_13undmehrwohnungen = (CASE WHEN bld.mfh_13undmehrwohnungen IN ('-', '–', '') THEN '0' ELSE bld.mfh_13undmehrwohnungen END)::bigint,
+    anderergebaeudetyp = (CASE WHEN bld.anderergebaeudetyp IN ('-', '–', '') THEN '0' ELSE bld.anderergebaeudetyp END)::bigint
 FROM {input_schema}.zensus_2022_100m_gebaeude_typ_groesse bld
 WHERE buildings_grid.x_mp = bld.x_mp_100m
   AND buildings_grid.y_mp = bld.y_mp_100m;
 
 -- Update with construction year data
 UPDATE {output_schema}.buildings_grid
-SET vor1919 = (CASE WHEN bauj.vor1919 IN ('-', '–', '') THEN '0' ELSE bauj.vor1919 END)::double precision,
-    a1919bis1948 = (CASE WHEN bauj.a1919bis1948 IN ('-', '–', '') THEN '0' ELSE bauj.a1919bis1948 END)::double precision,
-    a1949bis1978 = (CASE WHEN bauj.a1949bis1978 IN ('-', '–', '') THEN '0' ELSE bauj.a1949bis1978 END)::double precision,
-    a1979bis1990 = (CASE WHEN bauj.a1979bis1990 IN ('-', '–', '') THEN '0' ELSE bauj.a1979bis1990 END)::double precision,
-    a1991bis2000 = (CASE WHEN bauj.a1991bis2000 IN ('-', '–', '') THEN '0' ELSE bauj.a1991bis2000 END)::double precision,
-    a2001bis2010 = (CASE WHEN bauj.a2001bis2010 IN ('-', '–', '') THEN '0' ELSE bauj.a2001bis2010 END)::double precision,
-    a2011bis2019 = (CASE WHEN bauj.a2011bis2019 IN ('-', '–', '') THEN '0' ELSE bauj.a2011bis2019 END)::double precision,
-    a2020undspaeter = (CASE WHEN bauj.a2020undspaeter IN ('-', '–', '') THEN '0' ELSE bauj.a2020undspaeter END)::double precision
+SET vor1919 = (CASE WHEN bauj.vor1919 IN ('-', '–', '') THEN '0' ELSE bauj.vor1919 END)::bigint,
+    a1919bis1948 = (CASE WHEN bauj.a1919bis1948 IN ('-', '–', '') THEN '0' ELSE bauj.a1919bis1948 END)::bigint,
+    a1949bis1978 = (CASE WHEN bauj.a1949bis1978 IN ('-', '–', '') THEN '0' ELSE bauj.a1949bis1978 END)::bigint,
+    a1979bis1990 = (CASE WHEN bauj.a1979bis1990 IN ('-', '–', '') THEN '0' ELSE bauj.a1979bis1990 END)::bigint,
+    a1991bis2000 = (CASE WHEN bauj.a1991bis2000 IN ('-', '–', '') THEN '0' ELSE bauj.a1991bis2000 END)::bigint,
+    a2001bis2010 = (CASE WHEN bauj.a2001bis2010 IN ('-', '–', '') THEN '0' ELSE bauj.a2001bis2010 END)::bigint,
+    a2011bis2019 = (CASE WHEN bauj.a2011bis2019 IN ('-', '–', '') THEN '0' ELSE bauj.a2011bis2019 END)::bigint,
+    a2020undspaeter = (CASE WHEN bauj.a2020undspaeter IN ('-', '–', '') THEN '0' ELSE bauj.a2020undspaeter END)::bigint
 FROM {input_schema}.zensus_2022_100m_gebaeude_baujahr_mikrozensus bauj
 WHERE buildings_grid.x_mp = bauj.x_mp_100m
   AND buildings_grid.y_mp = bauj.y_mp_100m;
