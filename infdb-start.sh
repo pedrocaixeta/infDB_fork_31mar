acc0fd3b56a08b4fa7a03ac2fe6a127de773cf9d
Patrick Buchenberg <patrick.buchenberg@tum.de>
2026-01-09 10:37:11 +0100

    fix bug on startup because of .env and infdb-importer docker compose path

--- infdb-start.sh
+++ infdb-start.sh
@@ -5,17 +5,17 @@
 # infDB startup script
 # ----------------------------------------------------------------------
 
-# # Load environment variables from .env file
-set -a
-[ -f .env ] && . .env
-set +a
-
 # Check if .env file exists, if not create from template
 if [ ! -f .env ]; then
     echo "=== Creating .env from template ==="
     cp .env.template .env
     echo "=== .env file created. Please review and customize it as needed. ==="
 fi
+
+# Load environment variables from .env file
+set -a
+[ -f .env ] && . .env
+set +a
 
 # Pull latest images
 echo "=== Pull latest docker images ==="
