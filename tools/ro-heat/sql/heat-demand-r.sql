0885d4bfddead305a2ec982362909c56e170f68d
Patrick Buchenberg <patrick.buchenberg@tum.de>
2026-01-28 16:56:43 +0100

    Add SQL query for annual heat demand calculation based on temperature data

---
+++ tools/ro-heat/sql/heat-demand-r.sql
@@ -0,0 +1,25 @@
+SElECT
+    bldrc.building_objectid,
+    bldrc.resistance,
+    SUM(ts.value) as temp_sum,
+    count(ts.value) as count_temp,
+    (SUM(ts.value)-count(ts.value)*{temp_in})/bldrc.resistance as heat_demand_annual
+FROM
+    ro_heat.buildings_rc AS bldrc
+JOIN
+    basedata.bld2ts
+    ON bldrc.building_objectid = basedata.bld2ts.bld_objectid
+JOIN
+    opendata.openmeteo_ts_data AS ts
+    ON basedata.bld2ts.ts_metadata_id = ts.ts_metadata_id
+JOIN opendata.openmeteo_ts_metadata
+     ON ts.ts_metadata_id = opendata.openmeteo_ts_metadata.id
+JOIN opendata.buildings_lod2
+    ON bldrc.building_objectid = opendata.buildings_lod2.objectid
+WHERE
+    opendata.openmeteo_ts_metadata.name='openmeteo_hourly_temperature_2m'
+    AND ts.time  >= '{start_time}'
+    AND ts.time <  '{end_time}'
+    AND opendata.buildings_lod2.gemeindeschluessel LIKE '{ags}'
+    AND ts.value < {temp_in}
+GROUP BY bldrc.building_objectid, bldrc.resistance;
