0ccda7f0f2ae51b18e56bc2007aca71e82ec6669
bereba <beneharo.reveron-baecker@tum.de>
2026-01-29 03:03:12 +0000

    Fix environment path in .env

--- tools/infdb-basedata/run.sh
+++ tools/infdb-basedata/run.sh
@@ -2,9 +2,9 @@
 # e.g.: bash run.sh up, bash run.sh up --build, bash run.sh down, bash run.sh stop, ...
 set -e
 
-echo "Loading environment variables from infDB .env file..."
+echo "Loading environment variables from .env file..."
 set -a
-[ -f $(dirname "$0")/../.env ] && . $(dirname "$0")/../.env
+[ -f $(dirname "$0")/../../.env ] && . $(dirname "$0")/../../.env
 set +a
 
 echo "Loading environment variables from local .env file..."
