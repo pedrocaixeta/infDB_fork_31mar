8572d02cee184aab4ad8ee684f034a4b0d847f50
Patrick Buchenberg <patrick.buchenberg@tum.de>
2026-02-03 00:01:54 +0100

    Refactor building surface data query to utilize opendata tables for wall and roof surface area calculations

--- tools/ro-heat/sql/01_get_building_surface_data.sql
+++ tools/ro-heat/sql/01_get_building_surface_data.sql
@@ -1,23 +1,42 @@
-WITH wall_data AS (SELECT building_objectid,
-                          SUM(area) AS wall_surface_area
-                   FROM (SELECT regexp_replace(f.objectid, '_[^_]*-.*$', '') AS building_objectid,
-                                CAST(p.val_string AS double precision)       AS area
-                         FROM feature f
-                                  JOIN geometry_data gd ON f.id = gd.feature_id
-                                  JOIN property p ON gd.feature_id = p.feature_id
-                         WHERE f.objectclass_id = 709 -- WallSurface
-                           AND p.name = 'Flaeche') sub
-                   GROUP BY building_objectid),
-     roof_data AS (SELECT building_objectid,
-                          SUM(area) AS roof_surface_area
-                   FROM (SELECT regexp_replace(f.objectid, '_[^_]*-.*$', '') AS building_objectid,
-                                CAST(p.val_string AS double precision)       AS area
-                         FROM feature f
-                                  JOIN geometry_data gd ON f.id = gd.feature_id
-                                  JOIN property p ON gd.feature_id = p.feature_id
-                         WHERE f.objectclass_id = 712 -- RoofSurface
-                           AND p.name = 'Flaeche') sub
-                   GROUP BY building_objectid)
+-- WITH wall_data AS (SELECT building_objectid,
+--                           SUM(area) AS wall_surface_area
+--                    FROM (SELECT regexp_replace(f.objectid, '_[^_]*-.*$', '') AS building_objectid,
+--                                 CAST(p.val_string AS double precision)       AS area
+--                          FROM feature f
+--                                   JOIN geometry_data gd ON f.id = gd.feature_id
+--                                   JOIN property p ON gd.feature_id = p.feature_id
+--                          WHERE f.objectclass_id = 709 -- WallSurface
+--                            AND p.name = 'Flaeche') sub
+--                    GROUP BY building_objectid),
+--      roof_data AS (SELECT building_objectid,
+--                           SUM(area) AS roof_surface_area
+--                    FROM (SELECT regexp_replace(f.objectid, '_[^_]*-.*$', '') AS building_objectid,
+--                                 CAST(p.val_string AS double precision)       AS area
+--                          FROM feature f
+--                                   JOIN geometry_data gd ON f.id = gd.feature_id
+--                                   JOIN property p ON gd.feature_id = p.feature_id
+--                          WHERE f.objectclass_id = 712 -- RoofSurface
+--                            AND p.name = 'Flaeche') sub
+--                    GROUP BY building_objectid)
+WITH wall_data AS (
+        SELECT building_objectid,
+               SUM(area) AS wall_surface_area
+        FROM opendata.buildings_surfaces
+        JOIN opendata.buildings_lod2 ON opendata.buildings_surfaces.building_objectid = opendata.buildings_lod2.objectid
+        WHERE 
+                surface_type = 'WallSurface'
+                AND opendata.buildings_lod2.gemeindeschluessel LIKE '{ags}'
+        GROUP BY building_objectid),
+     roof_data AS (
+        SELECT building_objectid,
+               SUM(area) AS roof_surface_area
+        FROM opendata.buildings_surfaces
+        JOIN opendata.buildings_lod2 ON opendata.buildings_surfaces.building_objectid = opendata.buildings_lod2.objectid
+        WHERE 
+                surface_type = 'RoofSurface'
+                AND opendata.buildings_lod2.gemeindeschluessel LIKE '{ags}'
+        GROUP BY building_objectid)
+
 SELECT b.objectid                                                        AS building_objectid,
        b.floor_area,
        b.floor_number,
@@ -30,7 +49,7 @@ SELECT b.objectid                                                        AS buil
        -- Assume window area to be 0.2 m² per heated area
        b.floor_area * b.floor_number * 0.75 * 0.2                        AS window_area
 FROM {input_schema}.buildings b
-        LEFT JOIN wall_data wd
-ON b.objectid = wd.building_objectid
-    LEFT JOIN roof_data rd ON b.objectid = rd.building_objectid
-WHERE b.building_type IS NOT NULL
+        LEFT JOIN wall_data wd ON b.objectid = wd.building_objectid
+        LEFT JOIN roof_data rd ON b.objectid = rd.building_objectid
+WHERE b.building_type IS NOT NULL 
+  AND b.gemeindeschluessel LIKE '{ags}';
