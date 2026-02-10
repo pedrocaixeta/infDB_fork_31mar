8fe5f9d84522bd59a18f9bcecfe5b66de6c88ab4
Patrick Buchenberg <patrick.buchenberg@tum.de>
2026-02-10 15:31:07 +0100

    adjust building_view to old building_lod2 table

--- services/infdb-import/sql/sur.sql
+++ services/infdb-import/sql/sur.sql
@@ -48,11 +48,28 @@ DROP TABLE IF EXISTS {output_schema}.{bld_table_name}_view;
 CREATE MATERIALIZED VIEW {output_schema}.{bld_table_name}_view AS
 SELECT 
     bld.*,
+    ST_area(sur.geom) AS groundsurface_flaeche,
     sur.geom,
     ST_Centroid(sur.geom) AS centroid
 FROM {output_schema}.building_lod2 bld
 JOIN {output_schema}.{table_name} sur ON bld.objectid = sur.objectid
 WHERE sur.objectclass_id = 710; -- 710 = ground surface
+
 CREATE INDEX IF NOT EXISTS {bld_table_name}_view_objectid_idx ON {output_schema}.{bld_table_name}_view (objectid);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_id_idx ON {output_schema}.{bld_table_name}_view (id);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_ags_id_idx ON {output_schema}.{bld_table_name}_view (ags_id);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_feature_id_idx ON {output_schema}.{bld_table_name}_view (feature_id);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_gemeindeschluessel_idx ON {output_schema}.{bld_table_name}_view (gemeindeschluessel);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_objectclass_id_idx ON {output_schema}.{bld_table_name}_view (objectclass_id);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_height_idx ON {output_schema}.{bld_table_name}_view (height);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_storeysaboveground_idx ON {output_schema}.{bld_table_name}_view (storeysaboveground);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_building_function_code_idx ON {output_schema}.{bld_table_name}_view (building_function_code);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_zip_code_idx ON {output_schema}.{bld_table_name}_view (zip_code);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_street_idx ON {output_schema}.{bld_table_name}_view (street);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_house_number_idx ON {output_schema}.{bld_table_name}_view (house_number);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_city_idx ON {output_schema}.{bld_table_name}_view (city);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_country_idx ON {output_schema}.{bld_table_name}_view (country);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_state_idx ON {output_schema}.{bld_table_name}_view (state);
+CREATE INDEX IF NOT EXISTS {bld_table_name}_view_groundsurface_flaeche_idx ON {output_schema}.{bld_table_name}_view (groundsurface_flaeche);
 CREATE INDEX IF NOT EXISTS {bld_table_name}_view_geom_idx ON {output_schema}.{bld_table_name}_view USING GIST (geom);
 CREATE INDEX IF NOT EXISTS {bld_table_name}_view_centroid_idx ON {output_schema}.{bld_table_name}_view USING GIST (centroid);
