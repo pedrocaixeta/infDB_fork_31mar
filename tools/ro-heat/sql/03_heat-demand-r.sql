7a512139f883edd3d8d8b8cd70a894e0bae87bd1
Patrick Buchenberg <patrick.buchenberg@tum.de>
2026-03-02 14:29:58 -0800

    fix conversion bug of annual heat demand calculcaton in ro-heat

--- tools/ro-heat/sql/03_heat-demand-r.sql
+++ tools/ro-heat/sql/03_heat-demand-r.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS {output_schema}.annual_heating_demand (
 INSERT INTO {output_schema}.annual_heating_demand (building_objectid, "heating:demand[kWh]")
     SELECT
         bldrc.building_objectid,
-        (count(ts.value)*{temp_in}-SUM(ts.value))/bldrc.resistance/3600/1000 AS "heating:demand[kWh]"
+        -- simplified heat demand equation (6.4) in Patrick's phd thesis
+        (count(ts.value)*1/bldrc.resistance)*(avg(ts.value) - {temp_in})/1000 AS "heating:demand[kWh]"
     FROM
         {output_schema}.buildings_rc AS bldrc
     JOIN
@@ -22,11 +23,11 @@ INSERT INTO {output_schema}.annual_heating_demand (building_objectid, "heating:d
     JOIN basedata.buildings
         ON bldrc.building_objectid = basedata.buildings.objectid
     WHERE
+        ts.value < {temp_in} AND    -- constraint of upper equation (Tout < Tin) for all time steps
         opendata.openmeteo_ts_metadata.name = 'openmeteo_hourly_temperature_2m'
         AND ts.time  >= '{start_time}'
         AND ts.time <  '{end_time}'
         AND basedata.buildings.gemeindeschluessel LIKE '{ags}'
-        AND ts.value < {temp_in}
     GROUP BY bldrc.building_objectid, bldrc.resistance
 ON CONFLICT (building_objectid)
 DO UPDATE SET
